ab901b961f287b11be270dced780146950ba5260
penachoUA <penacho@ua.pt>
2024-05-29 15:53:53 +0100

    Add Blink for during score setting

--- Fase4/ConfController.vhd
+++ Fase4/ConfController.vhd
@@ -16,9 +16,13 @@ entity ConfController is
 end;
 
 architecture Structural of ConfController is
-	signal s_short_click  : std_logic;
-	signal s_long_click   : std_logic;
-	signal s_target       : std_logic_vector(5 downto 0);
+	signal s_short_click : std_logic;
+	signal s_long_click  : std_logic;
+	signal s_target      : std_logic_vector(5 downto 0);
+	
+	signal s_1hz_blink : std_logic;
+	signal s_hexTen    : std_logic_vector(6 downto 0);
+	signal s_hexUni    : std_logic_vector(6 downto 0);
 	
 	constant one_second   : std_logic_vector(31 downto 0) := x"02FAF080";
 begin
@@ -28,6 +32,13 @@ begin
 	HEX1 <= "0101011"; -- n
 	HEX0 <= "0001110"; -- F
 	----------------------------
+	
+	one_sec_blinker: entity work.BlinkGen(Behavioral)
+		generic map(STEPS => 50_000_000)
+		port map(
+			clk   => CLOCK_50,
+			blink => s_1hz_blink
+		);
 	
 	debounce1 : entity work.Debouncer(Behavioral)
      generic map(
@@ -53,7 +64,11 @@ begin
 	target_display: entity work.ScoreDisplay(Structural)
 		port map(
 			score  => '0' & s_target,
-			hexTen => HEX7,
-			hexUni => HEX6
+			hexTen => s_hexTen,
+			hexUni => s_hexUni
 		);
+		
+	HEX7 <= s_hexTen when s_1hz_blink = '1' else (others => 'Z');
+	HEX6 <= s_hexUni when s_1hz_blink = '1' else (others => 'Z');
+
 end;
