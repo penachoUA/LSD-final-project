dcc2aab591624eb1529b7bfe94d8bf09e13442df
penachoUA <penacho@ua.pt>
2024-06-01 13:26:36 +0100

    Add negative numbers to ScoreDisplay
    - Since I only need 2 numbers I decided to only handle
    the logic for these 2 numbers in the ScoreDisplay block.
    Making the counter signed didn't appeal to me since only
    the player scoring counters need negative counting

--- Fase6/ScoreDisplay.vhd
+++ Fase6/ScoreDisplay.vhd
@@ -12,12 +12,33 @@ entity ScoreDisplay is
 end;
 
 architecture Structural of ScoreDisplay is
-	signal s_bcd_uni : std_logic_vector(3 downto 0);
-	signal s_bcd_ten : std_logic_vector(3 downto 0);
+	signal s_hexTen    : std_logic_vector(6 downto 0);
+	signal s_hexUni    : std_logic_vector(6 downto 0);
+	
+	signal s_bcd_uni   : std_logic_vector(3 downto 0);
+	signal s_bcd_ten   : std_logic_vector(3 downto 0);
+	
+	-- Negative numbers display
+	constant s_minus_sign : std_logic_vector(6 downto 0) := "0111111";
+	signal s_score        : std_logic_vector(6 downto 0);
 begin
+	process(score)
+		begin
+			if score = "1111111" then
+				hexTen <= s_minus_sign;
+				s_score <= "0000001"; -- 1
+			elsif score = "1111110" then
+				hexTen <= s_minus_sign;
+				s_score <= "0000010"; -- 2
+			else
+				hexTen  <= s_hexTen;
+				s_score <= score;
+			end if;
+		end process;
+
 	bin2bcd: entity work.Bin2BCD(Behavioral)
 		port map(
-			binIn  => score,
+			binIn  => s_score,
 			bcdUni => s_bcd_uni,
 			bcdTen => s_bcd_ten
 		);
@@ -26,7 +47,7 @@ begin
 		port map(
 			enable   => '1',
 			binInput => s_bcd_ten,
-			decOut_n => hexTen
+			decOut_n => s_hexTen
 		);
 		
 	uni_7seg: entity work.Bin7SegDecoder(Behavioral)
